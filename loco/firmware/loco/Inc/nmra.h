656d4c4228f256ff2c452ea9774b9735b52f62fc
tnt23 <tim.tashpulatov@gmail.com>
2020-03-15 11:40:59 +0300

    Add decode code

---
+++ loco/firmware/loco/Inc/nmra.h
@@ -0,0 +1,34 @@
+#ifndef __NMRA_H__
+#define __NMRA_H__
+
+
+/* 	000 Decoder and Consist Control Instruction
+	001 Advanced Operation Instructions
+	010 Speed and Direction Instruction for reverse operation
+	011 Speed and Direction Instruction for forward operation
+	100 Function Group One Instruction
+	101 Function Group Two Instruction
+	110 Future Expansion
+	111 Configuration Variable Access Instruction
+*/
+
+#define INSTR_TYPE_BIT_MASK					0b11100000
+
+#define INSTR_DECODER_AND_CONSIST_CONTROL	0b00000000
+#define INSTR_ADVANCED_OPERATION			0b00100000
+#define INSTR_SPEED_DIR_REVERSE				0b01000000
+#define INSTR_SPEED_DIR_FORWARD				0b01100000
+#define INSTR_FUNCTION_GROUP_1				0b10000000
+#define INSTR_FUNCTION_GROUP_2				0b10100000
+#define INSTR_FEATURE_EXPANSION				0b11000000
+#define INSTR_CV_ACCESS						0b11100000
+
+#define INSTR_SPEED_BIT_MASK				0b00011111
+
+extern DCC_MSG Msg;
+
+void Decode ();
+
+
+
+#endif
---
+++ loco/firmware/loco/Src/nmra.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include "loco.h"
+#include "nmra.h"
+
+void Decode () {
+
+	switch (Msg.Data [2] & INSTR_TYPE_BIT_MASK) {
+		case INSTR_DECODER_AND_CONSIST_CONTROL:
+			break;
+
+		case INSTR_ADVANCED_OPERATION:
+			// 001CCCCC 0 DDDDDDDD
+				/* CCCCC = 11111: 128 Speed Step Control - Instruction "11111" is used to send one of 126 Digital Decoder speed
+				steps. The subsequent single byte shall define speed and direction with bit 7 being direction ("1" is forward and "0"
+				is reverse) and the remaining bits used to indicate speed. The most significant speed bit is bit 6. A data-byte value
+				of U0000000 is used for stop, and a data-byte value of U0000001 is used for emergency stop. This allows up to 126
+				speed steps.
+				*/
+			break;
+
+		case INSTR_SPEED_DIR_REVERSE:
+			// 010DDDDD
+
+			TIM3->CCR1 = Msg.Data [1] & INSTR_SPEED_BIT_MASK;
+
+
+			break;
+
+		case INSTR_SPEED_DIR_FORWARD:
+			// 011DDDDD
+			break;
+
+		case INSTR_FUNCTION_GROUP_1:
+			// 100DDDDD - FL and F1-F4
+			// If Bit 1 of CV#29 has a value of one (1), then bit 4 controls function FL,
+			// otherwise bit 4 has no meaning.
+			break;
+
+		case INSTR_FUNCTION_GROUP_2:
+			// 101SDDDD - F5-F12
+			break;
+
+		case INSTR_FEATURE_EXPANSION:
+			break;
+
+		case INSTR_CV_ACCESS:
+			// 1111CCCC 0 DDDDDDDD - short form
+			// 1110CCVV 0 VVVVVVVV 0 DDDDDDDD - long form
+			break;
+
+		default:
+			break;
+	}
+
+}
