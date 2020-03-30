259cefa10ffd984fea2ed66ed7eebc644d874670
Tim Tashpulatov <tashpulatov@arrival.com>
2020-03-30 19:18:28 +0300

    Update main.c

--- loco/firmware/loco/Src/main.c
+++ loco/firmware/loco/Src/main.c
@@ -34,22 +34,14 @@ static void MX_IWDG_Init (void);
 
 int main(void) {
   
-
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init ();
